dcb63949ef34a414588e5889106a637b96fdadd7
Greg Silverstein <greg.silverstein@gmail.com>
2023-04-30 21:30:38 -0400

    feat: explore CL repl

--- src/main.lisp
+++ src/main.lisp
@@ -3,3 +3,11 @@
 (in-package :cl-underfoot)
 
 ;; blah blah blah.
+(defun uf-printsome (input-var)
+  (format T "Hello Underfoot ~d ~%" input-var)
+  )
+
+(defun uf-loop-time ()
+  (loop for x in '(1 2 3 4 5 6 7 8 9 10)
+        do (sleep 2)(uf-printsome x))
+  )
